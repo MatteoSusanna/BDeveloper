4bd705da2d75c16433faf1cc10eedacb915af044
simone ricciardo <you@example.com>
2022-11-17 16:49:25 +0100

    seed

--- public/js/front.js
+++ public/js/front.js
@@ -19795,7 +19795,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\INTEL\Desktop\BDeveloper\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\simon\Desktop\bdeveloper\BDeveloper\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
